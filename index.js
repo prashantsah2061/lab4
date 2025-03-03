b31651a14ede4c8f819cd0012ad29a2e87998b4a
prashantsah2061 <psah@caldwell.edu>
2025-03-03 13:10:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from "public" directory
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Object to store page hits (resets on server restart)
+
 const hits = {};
 
 // API to track page visits
@@ -17,12 +17,12 @@ app.get('/hits/:pageId', (req, res) => {
     res.send(hits[pageId].toString());
 });
 
-// 404 Error Handling
+
 app.all('*', (req, res) => {
     res.status(404).send('Invalid URL.');
 });
 
-// Start server
+
 app.listen(PORT, () => {
     console.log(`✅ Server started at http://localhost:${PORT}`);
 });
